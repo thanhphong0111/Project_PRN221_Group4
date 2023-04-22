6ff4ee0c70fab5fe1f5d1823c2cb42cb9603b707
Yến Đỗ <126126234+thanhphong0111@users.noreply.github.com>
2023-04-23 04:03:59 +0700

    Frontend Shopping Cart
    Bo sung them wwwroot, _Layout1.cshtml cho customer va _Layout2.cshtml cho emp, thiet ke giao dien trang Shopping Cart

---
+++ WebRazor/Pages/Cart/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazor.Pages.Cart
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
