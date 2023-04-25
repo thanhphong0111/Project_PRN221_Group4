372988a9b0ee417e14fc3e6240f899079dd80b72
Yến Đỗ <126126234+thanhphong0111@users.noreply.github.com>
2023-04-25 21:07:30 +0700

    Update ShoppingCart
    Update hoan chinh ShoppingCart

---
+++ WebRazor/Helpers/SendMailHelper.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+
+namespace WebRazor.Helpers
+{
+	public class SendMailHelper : Controller
+	{
+        public async static void SendMail(string email, string body)
+        {
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "dangtdhe150020@fpt.edu.vn", 
+                    Password = "dang050401"
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                var message = new MailMessage();
+                message.To.Add(email);
+                message.Subject = "(no-reply)";
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.From = new MailAddress("dangtdhe150020@fpt.edu.vn");
+                await smtp.SendMailAsync(message);
+                
+            }
+        }
+        public async static void SendMail(string email, string body, Dictionary<Stream, string> files)
+        {
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "dangtdhe150020@fpt.edu.vn",
+                    Password = "dang050401"
+                };
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                var message = new MailMessage();
+                message.To.Add(email);
+                message.Subject = "(no-reply)";
+                message.Body = body;
+                message.IsBodyHtml = true;
+                message.From = new MailAddress("dangtdhe150020@fpt.edu.vn");
+                try
+                {
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file.Value);
+                        message.Attachments.Add(new Attachment(file.Key, file.Value));
+                    }
+                    await smtp.SendMailAsync(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+        }
+    }
+}
