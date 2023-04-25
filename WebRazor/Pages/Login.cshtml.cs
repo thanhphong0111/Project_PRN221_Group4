e7c52adca090b7044771c9c6f068a5b38b986e16
Hoangvippro <hoangnmhe153543@fpt.edu.vn>
2023-04-25 17:27:54 +0700

    Update_Login

--- WebRazor/Pages/Login.cshtml.cs
+++ WebRazor/Pages/Login.cshtml.cs
@@ -49,13 +49,22 @@ namespace WebRazor.Pages
                 return Page();
             }
 
-            Account account = context.Accounts.Include(a=>a.Customer).FirstOrDefault(u => u.Email.Equals(email));
+            Account account = context.Accounts.FirstOrDefault(u => u.Email.Equals(email));
 
-            account.Customer.Accounts = null;
-            if (account.Customer.Active == false)
+            if (account.Role == 1)// role employee
             {
-                ModelState.AddModelError("Error", "your account disabled!");
-                return Page();
+                account.Employee = context.Employees.FirstOrDefault(u => u.EmployeeId.Equals(account.EmployeeId));
+                account.Employee.Accounts = null;
+            }
+            else
+            {
+                account.Customer = context.Customers.FirstOrDefault(u => u.CustomerId.Equals(account.CustomerId));
+                account.Customer.Accounts = null;
+                if (account.Customer.Active == false)
+                {
+                    ModelState.AddModelError("Error", "your account disabled!");
+                    return Page();
+                }
             }
 
             string jsonAcount = JsonConvert.SerializeObject(account);
