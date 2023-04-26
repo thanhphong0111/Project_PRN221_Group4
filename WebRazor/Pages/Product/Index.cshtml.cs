2d0f3073fb6fa6e629f646a87b478e70183b0bbe
D U Y Đ Ă N G <dangtd5401@gmail.com>
2023-04-26 10:47:05 +0700

    paging list product

--- WebRazor/Pages/Product/Index.cshtml.cs
+++ WebRazor/Pages/Product/Index.cshtml.cs
@@ -26,42 +26,60 @@ namespace WebRazor.Pages.Product
 
         [FromQuery(Name = "id")]
         public string CatId { get; set; }
+
+        public const int pageSize = 12;
+        [BindProperty(SupportsGet = true, Name = "currentPage")]
+        public int currentPage { get; set; }
+        public int countPages { get; set; }
+
         public async Task OnGetAsync()
         {
             Categories = dbContext.Categories.ToList();
+            string search = "";
+            int totalProduct = getTotalProducts(search);
             var products = dbContext.Products;
+            countPages = (int)Math.Ceiling((double)totalProduct / pageSize);
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+            if (currentPage > countPages)
+            {
+                currentPage = countPages;
+            }
             if (CatId != null)
             {
-                Products = await products
-                    .Where(p => p.CategoryId == Int32.Parse(CatId))
+                Products = await (from p in dbContext.Products select p).Where(p => p.CategoryId == int.Parse(CatId))
+                    .Skip((currentPage - 1) * pageSize).Take(pageSize)
                     .ToListAsync();
             }
             else
             {
-                Products = await products.ToListAsync();
+                Products = await (from a in dbContext.Products orderby a.ProductId ascending select a).Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
             }
-
         }
         public async Task OnPostAsync()
         {
             string search = Request.Form["search"];
+            int totalProduct = getTotalProducts(search);
             ViewData["search"] = search;
             Categories = dbContext.Categories.ToList();
 
             var products = dbContext.Products;
             if (!string.IsNullOrEmpty(search))
             {
-                Products = await products.Where(p => p.ProductName == search).ToListAsync();
+                Products = await (from a in dbContext.Products orderby a.ProductId ascending select a).Where(a => a.ProductName.Contains(search)).Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
             }
             else if (CatId != null)
             {
-                Products = await products
-                    .Where(p => p.CategoryId == Int32.Parse(CatId))
-                    .ToListAsync();
+                Products = await (from p in dbContext.Products select p).Where(p => p.CategoryId == categoryChoose)
+                     .Skip((currentPage - 1) * pageSize).Take(pageSize)
+                     .ToListAsync();
             }
             else if (!string.IsNullOrEmpty(search) && CatId != null)
             {
-                Products = await products.Where(p => p.CategoryId == Int32.Parse(CatId) && p.ProductName == search).ToListAsync();
+                Products = await (from p in dbContext.Products select p).Where(p => p.CategoryId == categoryChoose && p.ProductName.Contains(search))
+                    .Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
             }
             else
             {
@@ -69,7 +87,23 @@ namespace WebRazor.Pages.Product
             }
 
         }
+        private int getTotalProducts(string? search)
+        {
+            var list = (from p in dbContext.Products select p).ToList();
 
+            if (!String.IsNullOrEmpty(search))
+            {
+                return list.Where(p => p.ProductName.ToLower().Contains(search.ToLower())).ToList().Count;
+            }
+            else if (categoryChoose > 0)
+            {
+                return list.Where(p => p.CategoryId == categoryChoose).ToList().Count;
+            }
+            else
+            {
+                return list.Count;
+            }
+        }
 
     }
 }
